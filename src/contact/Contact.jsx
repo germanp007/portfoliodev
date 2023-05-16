6e3e60303bb4b862cad24cd953203ee4804dc8c5
germanp007 <pintogerman281@gmail.com>
2023-05-15 23:11:49 -0300

    Terminando el portfolio

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -1,15 +1,23 @@
 import "./contact.css";
-import { useRef } from "react";
-import emailjs from "@emailjs/browser";
 import { Social } from "../hero/Social";
 import swal from "sweetalert";
+import emailjs from "@emailjs/browser";
+import { useForm } from "react-hook-form";
+import { useRef } from "react";
 
 export const Contact = () => {
   const form = useRef();
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { yes_i_understand: false },
+  });
+
   const sendEmail = (ev) => {
-    ev.preventDefault();
-    console.log(ev.target[0].value, ev.target[1].value, ev.target[2].value);
     emailjs
       .sendForm(
         "service_nqlh9pa",
@@ -19,24 +27,50 @@ export const Contact = () => {
       )
       .then(
         (result) => {
-          console.log(result);
-          form.current.reset();
+          console.log(result.text);
           swal({
             title: "Successful",
-            text: "Your message was sent successfully",
+            text: "The message was sent successfully.",
             icon: "success",
           });
         },
         (error) => {
           console.log(error.text);
-          swal({
-            title: "Error",
-            text: error.text,
-            icon: "error",
-          });
         }
       );
+    reset();
   };
+
+  // const sendEmail = (ev) => {
+  //   ev.preventDefault();
+
+  //   emailjs
+  //     .sendForm(
+  //       "service_nqlh9pa",
+  //       "template_aa6bemb",
+  //       ev.target,
+  //       "SODEE_LoWfgwMPNEH"
+  //     )
+  //     .then(
+  //       (result) => {
+  //         console.log(result);
+
+  //         swal({
+  //           title: "Successful",
+  //           text: "Your message was sent successfully",
+  //           icon: "success",
+  //         });
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //         swal({
+  //           title: "Error",
+  //           text: error.text,
+  //           icon: "error",
+  //         });
+  //       }
+  //     );
+  // };
 
   return (
     <>
@@ -47,17 +81,34 @@ export const Contact = () => {
           </h1>
         </div>
         <div className="form-container">
-          <form ref={form} onSubmit={sendEmail} className="form">
+          <form ref={form} onSubmit={handleSubmit(sendEmail)} className="form">
             <label>Name</label>
-            <input type="text" name="user_name" placeholder="John Doe" />
+            <input
+              type="text"
+              name="user_name"
+              placeholder="John Doe"
+              {...register("user_name", { required: true })}
+            />
+            {errors.user_name?.type === "required" && (
+              <span>Field Required</span>
+            )}
             <label>Email</label>
             <input
               type="email"
               name="user_email"
               placeholder="example@email.com"
+              {...register("user_email", { required: true })}
             />
+            {errors.user_email?.type === "required" && (
+              <span>Field Required</span>
+            )}
             <label>Message</label>
-            <textarea name="message" placeholder="Enter your message" />
+            <textarea
+              name="message"
+              placeholder="Enter your message"
+              {...register("message", { required: true })}
+            />
+            {errors.message?.type === "required" && <span>Field Required</span>}
             <input id="submit" type="submit" value="Send" />
           </form>
         </div>
