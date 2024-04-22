93a5b6e84e842fc6601a099fb15e8db016e23337
germanp007 <pintogerman281@gmail.com>
2024-04-22 17:28:34 -0300

    actualizando cv

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -5,7 +5,7 @@ import picture from "../assets/picture.png";
 import { HeroSvg } from "./HeroSvg";
 import "./hero.css";
 import { Social } from "./Social";
-import resume from "../pdf/germancv.pdf";
+import resume from "../pdf/GermanPintoCV.pdf";
 
 export const Hero = () => {
   useEffect(() => {
@@ -18,7 +18,7 @@ export const Hero = () => {
       <div className="presentation" data-aos="fade-up">
         <article>
           <h3>
-            Hello, <span style={{ color: "#26e6c6" }}>I'm</span>
+            Hello, <span style={{ color: "#26e6c6" }}>I&lsquo;m</span>
           </h3>
           <h1>
             German <span className="last_name">Pinto</span>
